063932c59066d2070a3c3978bb4e8d003a8ac285
ThiagoSere <tserebrinsky@mail.austral.edu.ar>
2026-08-10 17:04:00 -0300

    mejoras

--- parser/src/main/kotlin/printscript/parser/PrintScriptParser.kt
+++ parser/src/main/kotlin/printscript/parser/PrintScriptParser.kt
@@ -5,18 +5,21 @@ import printscript.parser.internal.expression.RecursiveDescentExpressionParser
 import printscript.parser.internal.statement.AssignmentParser
 import printscript.parser.internal.statement.DeclarationParser
 import printscript.parser.internal.statement.PrintlnParser
+import printscript.parser.internal.statement.StatementParser
 import printscript.statement.StatementSource
 import printscript.token.TokenSource
 
 class PrintScriptParser : Parser {
 
-    override fun parse(tokens: TokenSource): StatementSource {
+    override fun parse(tokens: TokenSource): StatementSource =
+        ParsingStatementSource(tokens, statementParsers())
+
+    private fun statementParsers(): List<StatementParser> {
         val expressionParser = RecursiveDescentExpressionParser()
-        val statementParsers = listOf(
+        return listOf(
             DeclarationParser(expressionParser),
             AssignmentParser(expressionParser),
             PrintlnParser(expressionParser),
         )
-        return ParsingStatementSource(tokens, statementParsers)
     }
 }
