89560a1a1b8f7a44be9d2102cc82db7aba763b0b
ThiagoSere <tserebrinsky@mail.austral.edu.ar>
2026-08-10 10:20:10 -0300

    dejarMasLindoElDeclarationParser

--- parser/src/main/kotlin/printscript/parser/internal/statement/DeclarationParser.kt
+++ parser/src/main/kotlin/printscript/parser/internal/statement/DeclarationParser.kt
@@ -10,95 +10,23 @@ import printscript.parser.internal.ParsingContext
 import printscript.parser.internal.ParsingResult
 import printscript.parser.internal.expression.ExpressionParser
 import printscript.statement.ParseError
+import printscript.token.Token
 import printscript.token.TokenType
 
 internal class DeclarationParser(
     private val expressionParser: ExpressionParser,
 ) : StatementParser {
 
-    override fun canStartWith(type: TokenType): Boolean {
-        return type == TokenType.LET
-    }
+    override fun canStartWith(type: TokenType): Boolean = type == TokenType.LET
 
-    override fun parse(
-        context: ParsingContext,
-    ): ParsingResult<Statement> {
-        val letToken = when (
-            val result = context.expect(TokenType.LET)
-        ) {
-            is ParsingResult.Success -> result.value
-            is ParsingResult.Failure -> return result
-        }
-
-        val identifierToken = when (
-            val result = context.expect(TokenType.IDENTIFIER)
-        ) {
-            is ParsingResult.Success -> result.value
-            is ParsingResult.Failure -> return result
-        }
-
-        when (val result = context.expect(TokenType.COLON)) {
-            is ParsingResult.Success -> Unit
-            is ParsingResult.Failure -> return result
-        }
-
-        val expectedTypes = setOf(
-            TokenType.NUMBER_TYPE,
-            TokenType.STRING_TYPE,
-        )
-
-        val typeToken = when (
-            val result = context.expect(expectedTypes)
-        ) {
-            is ParsingResult.Success -> result.value
-            is ParsingResult.Failure -> return result
-        }
-
-        val declaredType = when (typeToken.type) {
-            TokenType.NUMBER_TYPE -> DeclaredType.NUMBER
-            TokenType.STRING_TYPE -> DeclaredType.STRING
-
-            else -> {
-                return ParsingResult.Failure(
-                    ParseError.UnexpectedToken(
-                        expected = expectedTypes,
-                        actual = typeToken,
-                    ),
-                )
-            }
-        }
-
-        val nextToken = when (val result = context.peek()) {
-            is ParsingResult.Success -> result.value
-            is ParsingResult.Failure -> return result
-        }
-
-        val initializer: Expression? =
-            if (nextToken.type == TokenType.ASSIGN) {
-                when (
-                    val result =
-                        context.expect(TokenType.ASSIGN)
-                ) {
-                    is ParsingResult.Success -> Unit
-                    is ParsingResult.Failure -> return result
-                }
-
-                when (
-                    val result = expressionParser.parse(context)
-                ) {
-                    is ParsingResult.Success -> result.value
-                    is ParsingResult.Failure -> return result
-                }
-            } else {
-                null
-            }
-
-        val semicolonToken = when (
-            val result = context.expect(TokenType.SEMICOLON)
-        ) {
-            is ParsingResult.Success -> result.value
-            is ParsingResult.Failure -> return result
-        }
+    override fun parse(context: ParsingContext): ParsingResult<Statement> {
+        val letToken = context.expect(TokenType.LET).orReturn { return it }
+        val identifierToken = context.expect(TokenType.IDENTIFIER).orReturn { return it }
+        context.expect(TokenType.COLON).orReturn { return it }
+        val typeToken = context.expect(TYPE_TOKENS).orReturn { return it }
+        val declaredType = declaredTypeOf(typeToken).orReturn { return it }
+        val initializer = parseInitializer(context).orReturn { return it }
+        val semicolonToken = context.expect(TokenType.SEMICOLON).orReturn { return it }
 
         return ParsingResult.Success(
             VariableDeclarationStatement(
@@ -114,5 +42,37 @@ internal class DeclarationParser(
                 ),
             ),
         )
+    }
+
+    private fun declaredTypeOf(typeToken: Token): ParsingResult<DeclaredType> =
+        when (typeToken.type) {
+            TokenType.NUMBER_TYPE -> ParsingResult.Success(DeclaredType.NUMBER)
+            TokenType.STRING_TYPE -> ParsingResult.Success(DeclaredType.STRING)
+            else -> ParsingResult.Failure(
+                ParseError.UnexpectedToken(expected = TYPE_TOKENS, actual = typeToken),
+            )
+        }
+
+    private fun parseInitializer(context: ParsingContext): ParsingResult<Expression?> {
+        val nextToken = context.peek().orReturn { return it }
+        if (nextToken.type != TokenType.ASSIGN) {
+            return ParsingResult.Success(null)
+        }
+        context.expect(TokenType.ASSIGN).orReturn { return it }
+        return ParsingResult.Success(
+            expressionParser.parse(context).orReturn { return it },
+        )
+    }
+
+    private inline fun <T> ParsingResult<T>.orReturn(
+        onFailure: (ParsingResult.Failure) -> Nothing,
+    ): T =
+        when (this) {
+            is ParsingResult.Success -> value
+            is ParsingResult.Failure -> onFailure(this)
+        }
+
+    private companion object {
+        val TYPE_TOKENS = setOf(TokenType.NUMBER_TYPE, TokenType.STRING_TYPE)
     }
 }
