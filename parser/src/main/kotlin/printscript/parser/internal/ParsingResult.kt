741bb0ba7769c988b62a47192c007aeba4179ab4
ThiagoSere <tserebrinsky@mail.austral.edu.ar>
2026-08-20 15:18:17 -0300

    newParsingResult

--- parser/src/main/kotlin/printscript/parser/internal/ParsingResult.kt
+++ parser/src/main/kotlin/printscript/parser/internal/ParsingResult.kt
@@ -1,11 +1,12 @@
 package printscript.parser.internal
 
+import printscript.parser.internal.context.ParsingContext
 import printscript.statement.ParseError
 
 internal sealed interface ParsingResult<out T> {
-
-    data class Success<T>(
+    data class Success<out T>(
         val value: T,
+        val resultingContext: ParsingContext,
     ) : ParsingResult<T>
 
     data class Failure(
@@ -15,8 +16,8 @@ internal sealed interface ParsingResult<out T> {
 
 internal inline fun <T> ParsingResult<T>.orReturn(
     onFailure: (ParsingResult.Failure) -> Nothing,
-): T =
+): ParsingResult.Success<T> =
     when (this) {
-        is ParsingResult.Success -> value
+        is ParsingResult.Success -> this
         is ParsingResult.Failure -> onFailure(this)
     }
